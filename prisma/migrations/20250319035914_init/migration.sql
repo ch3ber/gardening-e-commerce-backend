bb7b4e1ca684e93db90bdeff0d2a4c4257fda32b
Eber Alejo <eber4202@hotmail.com>
2025-03-18 23:16:10 -0600

    ADD: prisma ORM

---
+++ prisma/migrations/20250319035914_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Producto" (
+    "id" SERIAL NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "descripcion" TEXT NOT NULL,
+    "precio" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Producto_pkey" PRIMARY KEY ("id")
+);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response } from 'express';
 import productosRouter from '@/routes/products';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use('/productos', productosRouter);
+
+app.get('/productosprisma', async (req, res) => {
+  const productos = await prisma.producto.findMany();
+  res.json(productos);
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Backend E-commerce funcionando!');
