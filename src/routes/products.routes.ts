2c3846a4ae0da64232a645c68e1b4e677b30e549
Eber Alejo <eber4202@hotmail.com>
2025-03-25 18:37:29 -0600

    ADD: validate middleware and validate products routes

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,15 +1,55 @@
 import { Router } from 'express';
 import * as ProductController from '@/controllers/products.controller';
 import { protect } from '@/middlewares/auth.middleware';
+import { validate } from '@/middlewares/validate.middleware';
+import { body, param } from 'express-validator';
 
 const router = Router();
 
 router.get('/', ProductController.getAll);
-router.get('/:id', ProductController.getById);
 
-// Proteger rutas no-GET
-router.post('/', protect, ProductController.create);
-router.put('/:id', protect, ProductController.update);
-router.delete('/:id', protect, ProductController.remove);
+router.get(
+  '/:id',
+  [param('id').isInt().withMessage('ID debe ser un número entero válido.')],
+  validate,
+  ProductController.getById,
+);
+
+// Proteger y validar rutas sensibles
+router.post(
+  '/',
+  protect,
+  [
+    body('nombre').notEmpty().withMessage('El nombre es requerido.'),
+    body('precio').isFloat({ min: 0 }).withMessage('Precio debe ser un número positivo.'),
+    body('stock').isInt({ min: 0 }).withMessage('Stock debe ser un número entero positivo.'),
+    body('descripcion').notEmpty().withMessage('La descripción es requerida.'),
+    body('marca').notEmpty().withMessage('La marca es requerida.'),
+  ],
+  validate,
+  ProductController.create,
+);
+
+router.put(
+  '/:id',
+  protect,
+  [
+    param('id').isInt().withMessage('ID debe ser un número entero válido.'),
+    body('nombre').optional().notEmpty(),
+    body('precio').optional().isFloat({ min: 0 }),
+    body('stock').optional().isInt({ min: 0 }),
+    body('descripcion').optional().notEmpty(),
+  ],
+  validate,
+  ProductController.update,
+);
+
+router.delete(
+  '/:id',
+  protect,
+  [param('id').isInt().withMessage('ID debe ser un número entero válido.')],
+  validate,
+  ProductController.remove,
+);
 
 export default router;
