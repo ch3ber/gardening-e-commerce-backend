759889f3e3db03af94780ce408d41a59326f197c
Eber Alejo <eber4202@hotmail.com>
2025-04-06 23:54:48 -0600

    UPDATE: include email in JWT token generation for enhanced user identification

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,6 +1,6 @@
 import { Role } from '@prisma/client';
 import jwt from 'jsonwebtoken';
 
-export const generateToken = (userId: number, role: Role) => {
-  return jwt.sign({ userId, role }, process.env.SECRET_JWT_KEY as string, { expiresIn: '7d' });
+export const generateToken = (userId: number, email: string, role: Role) => {
+  return jwt.sign({ userId, email, role }, process.env.SECRET_JWT_KEY as string, { expiresIn: '7d' });
 };
